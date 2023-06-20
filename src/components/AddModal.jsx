a903ff1b13421772c6982a6f84c4f56d020876a1
axel-ac <abdullahceylan.axel@gmail.com>
2023-06-20 13:59:12 +0300

    addmodal-form

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -1,23 +1,34 @@
-
-
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+
 export function AddModal({ show, handleClose }) {
   return (
     <>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title className="text-danger">Reservation for...</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="name">
+              <Form.Label>Patient Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter your name" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="datetime">
+              <Form.Label>Day&Time</Form.Label>
+              <Form.Control type="datetime-local" />
+            </Form.Group>
+            <div className="text-center">
+            <Button variant="success" type="submit" className="me-2">
+              Submit
+            </Button>
+          <Button variant="danger" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
+            </div>
+          </Form>
+        </Modal.Body>
       </Modal>
     </>
   );
